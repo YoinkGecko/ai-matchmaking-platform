aba775572c47ee012f52071f9129606793617443
Kartikeya <kartikeya.anjul@gmail.com>
2026-09-20 23:20:04 +0530

    refactor(requirements) removed embedding bugs

--- backend/src/services/requirement.service.ts
+++ backend/src/services/requirement.service.ts
@@ -49,7 +49,7 @@ export const createRequirement = async (data: CreateRequirementInput) => {
   VALUES (
     $1, $2, $3, $4, $5,
     $6, $7, $8, $9, $10,
-    $11, $12, $13, $14
+    $11, $12, $13, $14::vector
   )
   RETURNING *;
   `;
